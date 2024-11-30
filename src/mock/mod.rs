aa33ee4de8ae583b6f2a37eb4c2efdd819af40a1
intothedark.me@gmail.com <intothedark.me@gmail.com>
2024-11-30 01:38:08 -0800

    Save

--- src/mock/mod.rs
+++ src/mock/mod.rs
@@ -1,12 +1,21 @@
-use crate::plib::db::InstalledVersion;
+use crate::plib::db::{InstallType, InstalledVersion};
 
 pub fn get_mocked_install_versions(number: u64) -> Vec<InstalledVersion> {
     let b_string = "ProtonGE-".to_owned();
     let mut vec: Vec<InstalledVersion> = Vec::new();
     for x in 0..number {
+        let mut install_type = InstallType::WINE;
+        if x % 3 == 1 {
+            install_type = InstallType::WINE
+        } else if x % 3 == 2 {
+            install_type = InstallType::PROTON
+        } else {
+            install_type = InstallType::CUSTOM
+        }
         let v = InstalledVersion {
             name: format!("{}{}", b_string, x),
             location: "/path/to/the/file".to_owned(),
+            install_type,
         };
         vec.push(v);
     }
--- src/plib/db.rs
+++ src/plib/db.rs
@@ -3,17 +3,30 @@ use crate::plib::fsutils::get_db_path;
 use rusqlite;
 use std::sync::Arc;
 
+pub enum InstallType {
+    WINE,
+    UMU,
+    PROTON,
+    CUSTOM,
+}
+
 pub struct InstalledVersion {
     // Name of the install. In most cases this will be the same as the version or asset name
     // We will allow the user to change this
     pub name: String,
     // Location of the install. This can be edited from the homepage
     pub location: String,
+    // The type of install. This is meant to be used primarily for filtering.
+    pub install_type: InstallType,
 }
 
 impl InstalledVersion {
-    pub fn new(name: String, location: String) -> Self {
-        Self { name, location }
+    pub fn new(name: String, location: String, install_type: InstallType) -> Self {
+        Self {
+            name,
+            location,
+            install_type,
+        }
     }
 }
 
@@ -37,7 +50,7 @@ impl AppDB {
 
 impl AppDB {
     pub fn create_db_or_do_nothing(&self) -> Result<(), ProtonCtlError> {
-        let create_table_sql = r#"CREATE TABLE IF NOT EXISTS installs ( id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, location TEXT NOT NULL);"#;
+        let create_table_sql = r#"CREATE TABLE IF NOT EXISTS installs ( id INTEGER PRIMARY KEY, install_type TEXT NOT NULL, name TEXT NOT NULL UNIQUE, location TEXT NOT NULL);"#;
         if let Err(e) = self.db_connection.execute(create_table_sql, []) {
             return Err(ProtonCtlError::from(e));
         }
@@ -45,13 +58,23 @@ impl AppDB {
     }
 
     pub fn get_entries(&self) -> Result<Vec<InstalledVersion>, ProtonCtlError> {
-        let get_entries = r#"SELECT name, location FROM installs"#;
+        let get_entries = r#"SELECT name, location, install_type FROM installs"#;
         let mut stmt = self.db_connection.prepare(get_entries)?;
         let entries = stmt
             .query_map([], |row| {
+                let mut install_type = InstallType::PROTON;
+                let install_type_text: String = row.get(2)?;
+                if (install_type_text == "WINE") {
+                    install_type = InstallType::WINE
+                } else if (install_type_text == "PROTON") {
+                    install_type = InstallType::PROTON
+                } else {
+                    install_type = InstallType::CUSTOM
+                }
                 Ok(InstalledVersion {
                     name: row.get(0)?,
                     location: row.get(1)?,
+                    install_type,
                 })
             })?
             .collect::<Result<Vec<_>, _>>()?;
